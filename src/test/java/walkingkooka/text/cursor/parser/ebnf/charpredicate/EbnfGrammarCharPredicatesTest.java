7183d5f8bd5ec0f7fc6994d6585b9be22674fa4e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-18 15:14:10 +1000

    https://github.com/mP1/walkingkooka/pull/3326 PublicStaticHelperTesting extends PublicClassTesting
    - https://github.com/mP1/walkingkooka/pull/3326
    - PublicStaticHelperTesting extends PublicClassTesting

--- src/test/java/walkingkooka/text/cursor/parser/ebnf/charpredicate/EbnfGrammarCharPredicatesTest.java
+++ src/test/java/walkingkooka/text/cursor/parser/ebnf/charpredicate/EbnfGrammarCharPredicatesTest.java
@@ -17,7 +17,6 @@
 
 package walkingkooka.text.cursor.parser.ebnf.charpredicate;
 
-import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.reflect.PublicStaticHelperTesting;
 
 import java.lang.reflect.Method;
@@ -32,10 +31,5 @@ public final class EbnfGrammarCharPredicatesTest implements PublicStaticHelperTe
     @Override
     public boolean canHavePublicTypes(final Method method) {
         return false;
-    }
-
-    @Override
-    public JavaVisibility typeVisibility() {
-        return JavaVisibility.PUBLIC;
     }
 }
